d6f25af8282c372434ab32fce1920ab40d430933
Aadi Yogakar <ayogakar@gmail.com>
2015-05-19 18:03:00 +0530

    getcode in video.php changed

--- lmsin/corporate/js/cynets_modal.js
+++ lmsin/corporate/js/cynets_modal.js
@@ -23,9 +23,9 @@ function CynetsModel(element, options)
 {
 	$(".modal-backdrop").remove();
 	$(".modal").remove();
-	var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button><h4 class="modal-title" id="myModalLabel">'+options.title+' </h4></div><div class="modal-body">'+element+'</div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Submit changes</button></div></div></div>';
+	var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button><h4 class="text-center modal-title" id="myModalLabel">'+options.title+' </h4></div><a href="http://localhost/'+element+'" target="_blank">Click me</a><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
 	
-	var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog '+options.modal_size+'"><div class="modal-content "> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button><h4 class="modal-title" id="myModalLabel">'+options.title+' </h4></div><div class="modal-body">'+element+'</div></div></div>';
+	//var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog '+options.modal_size+'"><div class="modal-content "> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button><h4 class="modal-title" id="myModalLabel">'+options.title+' </h4></div><div class="modal-body">'+element+'</div></div></div>';
 	
 	$(WRAPPER).appendTo(document.body);
 	$('#myModal').modal({ backdrop:options.backdrop,keybord:options.keybord})
