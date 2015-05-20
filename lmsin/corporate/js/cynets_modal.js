29c518eef38ad77633c06ac6b5301b94b15e22a5
Aadi Yogakar <ayogakar@gmail.com>
2015-05-20 10:28:10 +0530

    Get Code modified
    text displayed and link added
    cross icon removed

--- lmsin/corporate/js/cynets_modal.js
+++ lmsin/corporate/js/cynets_modal.js
@@ -12,7 +12,7 @@ function cn_window_open(title,data,is_closable,size)
 	hc["modal_size"]=modal_size;
 	
 	hc['backdrop']=false;
-	hc['keybord']=true;
+	hc['keyboard']=true;
 	hc["closeable"]=is_closable;
 	hc["closeText"]="<img border=\"0\" src=\""+SERVER_PATH+"images/close.png\" />";
 	win = new CynetsModel(data, hc);
@@ -23,8 +23,26 @@ function CynetsModel(element, options)
 {
 	$(".modal-backdrop").remove();
 	$(".modal").remove();
-	var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button><h4 class="text-center modal-title" id="myModalLabel">'+options.title+' </h4></div><a href="http://localhost/'+element+'" target="_blank">Click me</a><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
-	
+	var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true">\n\
+                        <div class="modal-dialog">\n\
+                            <div class="modal-content">\n\
+                                <div class="modal-header">\n\
+                                    <h3 class="text-center modal-title" id="myModalLabel">'+options.title+' </h3>\n\
+                                </div>\n\
+                                <br>\n\
+                                <br>\n\
+                                <h4 class="text-center">http://localhost/'+element+'</h4>\n\
+                                <br>\n\
+                                <br>\n\
+                                <h4 class="text-center">Click <a href="http://localhost/'+element+'" target="_blank">here</a> to go to the page</h4>\n\
+                                <br>\n\
+                                <br>\n\
+                                <div class="modal-footer">\n\
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\n\
+                                </div>\n\
+                            </div>\n\
+                        </div>\n\
+                    </div>';
 	//var WRAPPER='<div class="modal fade" id="myModal" tabindex="-1" role="dialog"  aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog '+options.modal_size+'"><div class="modal-content "> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button><h4 class="modal-title" id="myModalLabel">'+options.title+' </h4></div><div class="modal-body">'+element+'</div></div></div>';
 	
 	$(WRAPPER).appendTo(document.body);
